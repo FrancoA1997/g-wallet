fba620ec2f13946ee71eda7c7c5649dff6a5593d
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2022-11-24 19:19:32 -0300

    overall improvements

--- wallet-frontend/src/app/services/helper.ts
+++ wallet-frontend/src/app/services/helper.ts
@@ -1,3 +1,3 @@
-let baseUrl = '//gwalletserver.us-east-1.elasticbeanstalk.com'
-let _baseUrl = 'http://localhost:8080'
+let _baseUrl = '//gwalletserver.us-east-1.elasticbeanstalk.com'
+let baseUrl = 'http://localhost:8080/'
 export default baseUrl;
