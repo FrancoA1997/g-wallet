f18170751d32ec8850bc7574118f41457914d675
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2023-02-01 18:49:30 -0300

    routing update

--- wallet-frontend/src/app/app-routing.module.ts
+++ wallet-frontend/src/app/app-routing.module.ts
@@ -20,6 +20,7 @@ const routes: Routes = [
   {
     path : 'home',
     component : HomeComponent,
+    runGuardsAndResolvers: "always",
     pathMatch : "full",
     canActivate : [UserGuard],
   },
